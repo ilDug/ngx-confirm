a72caa87cdce5688ac4cb85e9b4a370d220614a5
ilDug <dognini.marco@gmail.com>
2022-11-18 21:48:46 +0100

    transfer DEMO

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content">
+        <button mat-raised-button color="primary" dagConfirm="Are you Sure?" (confirm)="doSomething()">open</button>
+        <p>{{result | json}}</p>
+    </div>
+
+  `,
+    styles: []
 })
 export class AppComponent {
-  title = 'demo';
+    result: boolean = false;
+
+    doSomething() {
+        this.result = !this.result
+    }
 }
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxConfirmModule } from 'ngx-confirm';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      MatButtonModule,
+      NgxConfirmModule,
+      BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
