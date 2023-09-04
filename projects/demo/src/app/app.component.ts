4445d335c9ad36d716b92c8a7dd4b2568a165589
ilDug <dognini.marco@gmail.com>
2023-09-04 20:57:17 +0200

    iniziata la creazione della pages demo

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,20 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-        <button mat-raised-button color="primary" dagConfirm="Are you Sure?" (confirm)="doSomething()">open</button>
-        <p>{{result | json}}</p>
-    </div>
-
-  `,
-    styles: []
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    result: boolean = false;
 
-    doSomething() {
-        this.result = !this.result
-    }
 }
