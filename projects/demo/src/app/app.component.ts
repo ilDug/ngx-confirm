05c55bd83b288b0342d9477dd9a02a6df74c2f71
ilDug <dognini.marco@gmail.com>
2023-09-05 21:41:05 +0200

    Demo page completed

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+    public confirmationMsg: string
+
+    doSomething() {
+        this.confirmationMsg = "You have confirmed!"
+    }
+
+    reset() {
+        this.confirmationMsg = "Please, Confirm!";
+    }
+
+    ngOnInit(): void {
+        this.reset();
+    }
 }
