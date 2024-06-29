29930f2d113c82e9ea51ad2c64d6c43279daa83f
ilDug <dognini.marco@gmail.com>
2024-06-29 15:46:17 +0200

    completed the library

--- projects/ngx-confirm/src/lib/confirm-dialog.component.ts
+++ projects/ngx-confirm/src/lib/confirm-dialog.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
-  selector: 'dag-confirm-dialog',
-  standalone: true,
-  imports: [],
-  templateUrl: './confirm-dialog.component.html',
-  styles: ``
+    selector: 'dag-confirm-dialog',
+    standalone: true,
+    imports: [MatDialogModule],
+    templateUrl: './confirm-dialog.component.html',
+    styles: ``
 })
 export class ConfirmDialogComponent {
-
+    message: string = inject(MAT_DIALOG_DATA);
 }
--- projects/ngx-confirm/src/lib/confirm.directive.ts
+++ projects/ngx-confirm/src/lib/confirm.directive.ts
@@ -1,11 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, inject, input, output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from './confirm-dialog.component';
 
 @Directive({
     selector: '[ngxConfirm], [dagConfirm]',
-    standalone: true
+    standalone: true,
+    host: {
+        '(click)': 'openDialog()', // listener per l'evento click
+        '[style.cursor]': '"pointer"' // imposta il cursore a "pointer" quando si passa sopra l'elemento
+    }
 })
 export class ConfirmDirective {
+    private dialog = inject(MatDialog);
+    ngxConfirm = input<string>(null, { alias: 'dagConfirm' }); //messaggio di conferma
+    confirm = output<boolean>(); //emette un valore booleano che triggera l'azione passata 
 
-    constructor() { }
-
+    openDialog() {
+        this.dialog.open(ConfirmDialogComponent, { data: this.ngxConfirm })
+            .afterClosed()
+            .subscribe(
+                res => res ? this.confirm.emit(true) : null
+            )
+    }
 }
